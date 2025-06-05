d5354e8da3e38edad61a4a11dd72eb8799885d9a
Annie <annenark@gmail.com>
2025-06-05 09:54:27 -0500

    Added cat to click on as well as changed back to home

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -12,10 +12,24 @@ struct ContentView: View {
         NavigationStack {
             VStack {
                 Text("This is the root view 🌳")
-                NavigationLink(destination: Text("You've arrived to the Second View 🎊")) {
+                NavigationLink(destination: SecondView()) {
                     Text("Click Me!")
+                        .font(.title)
+                        .foregroundColor(Color.black)
+                    
+                
+                }
+            
+                NavigationLink(destination: Text("Why did you click on a cat?")) {
+                    Image("calicat")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 150.0)
                 }
             }
+            .navigationTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarHidden(true)
         }
 
     }
